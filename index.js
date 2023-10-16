d2fb122e8108b087c59f108d09f0a671b4a8c42c
Ashley Rarieya <ararieya@gmail.com>
2023-10-16 16:26:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution in this file!
+let customerName;
+
+function upperCaseCustomerName() {
+    if (customerName) {
+        customerName = customerName.toUpperCase();
+    }
+}
+
+function setBestCustomer() {
+    bestCustomer = 'Essy';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'Michelle';
+}
+
+const leastFavoriteCustomer = 'Alice';
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'New Least Favorite';
+}
+
+customerName = 'Ashley';
+console.log('Original customerName:', customerName);
+
+upperCaseCustomerName();
+console.log('Uppercased customerName:', customerName); 
+
+setBestCustomer();
+console.log('bestCustomer:', bestCustomer);
+
+overwriteBestCustomer();
+console.log('bestCustomer (after overwrite):', bestCustomer); 
+
+console.log('leastFavoriteCustomer:', leastFavoriteCustomer);
+
+
+
